6ac9e74c0fa180d4a700be62009edb7bdcaa83da
bootjp / yoshiaki.ueda <oh@bootjp.me>
2019-07-11 20:10:12 +0900

    initial commit

---
+++ cli.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/kataras/iris/cache"
+
+	"github.com/kataras/iris"
+)
+
+func main() {
+
+	app := iris.Default()
+
+	// response content and no-cache
+	// It did not go well as a precedent case  https://twitter.com/27Cobalter/status/1149185587594723328
+	app.Get("/no-cache", func(ctx iris.Context) {
+		ctx.Header("cache-control", "no-cache")
+		// TODO : add response image
+	})
+
+	// app.he
+	app.Get("/redirect", func(ctx iris.Context) {
+		ctx.Header(cache.CacheControlHeaderValue, "no-cache")
+		t := time.Now()
+		ctx.Redirect("/img/t?="+t.UTC().String(), 307)
+	})
+
+	// always return 404 and contents.
+	app.Get("/404", func(ctx iris.Context) {
+		ctx.Header(cache.CacheControlHeaderValue, "no-cache")
+		ctx.StatusCode(404)
+		// TODO : add response image
+	})
+
+	if err := app.Run(iris.Addr(":8080")); err != nil {
+		log.Fatal(err)
+	}
+}
