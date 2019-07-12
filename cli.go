efba8fe104f82a0d966dcff5452298a0453cb587
bootjp / yoshiaki.ueda <oh@bootjp.me>
2019-07-12 10:53:03 +0900

    no-cache change to no-store

--- cli.go
+++ cli.go
@@ -16,20 +16,20 @@ func main() {
 	// response content and no-cache
 	// It did not go well as a precedent case  https://twitter.com/27Cobalter/status/1149185587594723328
 	app.Get("/no-cache", func(ctx iris.Context) {
-		ctx.Header("cache-control", "no-cache")
+		ctx.Header("cache-control", "no-store")
 		// TODO : add response image
 	})
 
 	// app.he
 	app.Get("/redirect", func(ctx iris.Context) {
-		ctx.Header(cache.CacheControlHeaderValue, "no-cache")
+		ctx.Header(cache.CacheControlHeaderValue, "no-store")
 		t := time.Now()
 		ctx.Redirect("/img/t?="+t.UTC().String(), 307)
 	})
 
 	// always return 404 and contents.
 	app.Get("/404", func(ctx iris.Context) {
-		ctx.Header(cache.CacheControlHeaderValue, "no-cache")
+		ctx.Header(cache.CacheControlHeaderValue, "no-store")
 		ctx.StatusCode(404)
 		// TODO : add response image
 	})
